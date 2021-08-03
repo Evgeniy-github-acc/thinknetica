0163b2d49e870c1557095db80868be5b71c1d22e
Evgeniy-github-acc <3374555@gmail.com>
2021-08-03 13:13:02 +0300

    add carriage_fits? method

--- trains/train.rb
+++ trains/train.rb
@@ -1,62 +1,38 @@
 #   Класс Train (Поезд):
 class Train
-    attr_reader :number, :size, :type
-    attr_accessor :speed, :route
 
-    def initialize (number, type, size) # Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов, 
-        @number = number                # эти данные указываются при создании экземпляра класса
-        @type = type
-        @size = size
+    attr_reader :number, :speed, :type 
+    attr_accessor :route, :carriages
+    attr_writer :speed
+
+    def initialize (number)             
+        @number = number                
         @speed = 0
-    end
-    
-
-    def stop
-        self.speed = 0    # Может тормозить (сбрасывать скорость до нуля)
-    end
-    
-    def add_carriage 
-      if @speed == 0                   #  Может прицеплять/отцеплять вагоны (по одному вагону за операцию, 
-        @size += 1                     #  метод просто увеличивает или уменьшает количество вагонов). 
-      else puts "You must stop first"  # Прицепка/отцепка вагонов может осуществляться только если поезд не движется.
-      end  
-    end    
-    
-    def cut_carriage
-        if size > 0 
-           if @speed == 0
-               @size -= 1
-               else puts "You must stop first"  
-           end
-        else "There is no carriages"
-        end  
-      end
-
-      
-    def set_route(route)                    # Может принимать маршрут следования (объект класса Route) 
-        @route = route
-        @station = route.first_station         # При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
-        @station.train_arrive (self) 
+        @carriages = []
+        @type = 0
     end
 
-    def go_forward 
-        next_station = get_stations.last
-        if next_station
-            @station.train_depature self, next_station          # Может перемещаться между станциями, указанными в маршруте.
-        else                                                    # Перемещение возможно вперед и назад, но только на 1 станцию за раз.
-          puts "The last station! Go back or set the new route"
+    def carriage_fits? carriage
+        if carriage.type == self.type
+            true 
+        else
+            false
         end
-    end   
+    end
+    
+    def add_carriage carriage
+        if carriage_fits? carriage  
+            if @speed == 0                           
+                @carriages << carriage                  
+            else puts "You must stop first"           
+            end
+        else
+            puts "You can't use this type of carriage for this train"
+         end  
+    end
 
-    def go_back
-        previous_station = get_stations.first
-        if previous_station
-            @station.train_depature self, previous_station
-        else            
-          puts "The first station! Go forward or set the new route"
-        end
-    end   
-
+ 
+  
     def get_stations                                                # Возвращать предыдущую станцию, текущую, следующую, на основе маршрута                            
         current_station = @station  
         if current_station == route.last_station
@@ -71,5 +47,50 @@ class Train
                     
         [previous_station, current_station, next_station]
     end
+
+   
+   
+    def stop
+        self.speed = 0                          
+    end
+    
+  
+    
+    def cut_carriage carriage
+        if @carriages.size > 0 
+           if @speed == 0
+               carriages.delete carriage
+               else puts "You must stop first"  
+           end
+        else puts "There is no carriages"
+        end  
+    end
+     
+      
+    def set_route(route)                    
+        @route = route
+        @station = route.first_station         
+        @station.train_arrive (self) 
+    end
+
+    def go_forward 
+        next_station = get_stations.last
+        if next_station
+            @station.train_depature self, next_station          
+        else                                                    
+          puts "The last station! Go back or set the new route"
+        end
+    end   
+
+    def go_back
+        previous_station = get_stations.first
+        if previous_station
+            @station.train_depature self, previous_station
+        else            
+          puts "The first station! Go forward or set the new route"
+        end
+    end   
+ 
+    
 end
 
