f54daa68af4210ece1dd36846b6c777cb493bafe
Evgeniy-github-acc <3374555@gmail.com>
2021-08-03 09:41:16 +0300

    create cars and create trains methods added

--- trains/main_menu.rb
+++ trains/main_menu.rb
@@ -28,19 +28,25 @@ class TrainMenu < Menu
 
     def initialize
         @trains = []
+        @cars = []
     end
 
     def show_trains
         @trains.each {|train| puts "№ #{train.number}, type #{train.type.to_s}, cars #{train.carriages.length}"}
+    end
+
+    def show_cars
+        @cars.each {|car| puts "№  #{car.number}, type #{car.type.to_s}"}
     end
     
     def set_items
         items = {
             1 => 'Create train',
             2 => 'Add cars',
-            3 => 'Delete cars',
-            4 => 'Move train',
-            5 => 'Previous menu',
+            3 => 'Create cars',
+            4 => 'Delete cars',
+            5 => 'Move train',
+            6 => 'Previous menu',
         }
     end
 
@@ -68,14 +74,52 @@ class TrainMenu < Menu
             else puts "Wrong type"
             end
         @trains << train
-        navigate
+        self.navigate
     end
+
+    def add_cars
+        if @cars.empty? || @trains.empty?
+            puts "Create trains and cars first"
+            gets
+            self.navigate
+        end   
+        show_trains
+        show_cars
+        puts "Select train"
+        train_list_number = gets.chomp.to_i
+        car_list_number = gets.chomp.to_i
+       
+    end
+
+    def create_cars
+        show_cars if @cars
+        puts "Enter carriage number"
+        number = gets.chomp
+        puts "Enter carriage type
+        1. Cargo
+        2. Passenger"
+        type = gets.chomp
+        if type == '1'
+            car = CarrigeCargo.new number
+        elsif type == '2'
+            car = CarrigePassenger.new number
+        else puts "Wrong type"
+        end
+        @cars << car
+        show_cars
+        self.navigate
+    end
+
     
     def navigate 
         choise = get_choise
         case choise
             when '1'
             create_train
+            when '2'
+            add_cars
+            when '3'
+            create_cars
         end    
     end    
 
