adf57bbe7143055d63c75d3cf9b43f194dd69b38
Evgeniy-github-acc <3374555@gmail.com>
2021-08-04 22:28:57 +0300

    station.trains_list problem fixed

--- trains/station.rb
+++ trains/station.rb
@@ -2,14 +2,15 @@
 # Класс Station (Станция):
 
 class Station
-    attr_reader :trains_list, :name
+    attr_reader :name
+    attr_accessor :trains_list
 
     def initialize (name)
         @name = name        # Имеет название, которое указывается при ее создании
         @trains_list = []   # Может возвращать список всех поездов на станции, находящиеся в текущий момент
     end   
     
-    def train_arrive (train)
+    def train_arrive train
         @trains_list << train   #Может принимать поезда (по одному за раз)
     end
 
@@ -17,15 +18,14 @@ class Station
         @trains_list.each{|train| puts train.number} 
     end    
 
-    def train_depature (train, station)    #Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
+    def train_depature (train)    #Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
         @trains_list.delete train
-        train.station = station
     end    
 
     def trains_types
        passenger = @trains_list.select{|train| train.type == :passenger}  #Может возвращать список поездов на станции по типу кол-во грузовых, пассажирских
        cargo = @trains_list.select{|train| train.type == :cargo}
-       passenger.each {|i| puts i.number}
-       cargo.each {|i| puts i.number}
+       passenger.each {|i| puts "Passenger #{i.number}"}
+       cargo.each {|i| puts "Cargo #{i.number}"}
     end
 end
--- trains/train.rb
+++ trains/train.rb
@@ -1,11 +1,10 @@
 #   Класс Train (Поезд):
 class Train
 
-    attr_reader :number, :speed, :type 
-    attr_accessor :route, :carriages
-    attr_writer :speed
-
-    def initialize (number)             
+    attr_reader :number, :type 
+    attr_accessor :route, :carriages, :station, :speed
+    
+   def initialize (number)             
         @number = number                
         @speed = 0
         @carriages = []
@@ -31,31 +30,11 @@ class Train
          end  
     end
 
- 
-  
-    def get_stations                                                # Возвращать предыдущую станцию, текущую, следующую, на основе маршрута                            
-        current_station = @station  
-        if current_station == route.last_station
-            next_station = nil
-        else next_station = @route.stations[@route.stations.index(@station)+1]
-        end
-
-        if current_station == route.first_station
-            previous_station = nil
-        else  previous_station = @route.stations[@route.stations.index(@station)-1]
-        end
-                    
-        [previous_station, current_station, next_station]
-    end
-
-   
-   
+       
     def stop
         self.speed = 0                          
     end
-    
-  
-    
+        
     def cut_carriage carriage
         if @carriages.size > 0 
            if @speed == 0
@@ -65,32 +44,51 @@ class Train
         else puts "There is no carriages"
         end  
     end
-     
-      
-    def set_route(route)                    
+ 
+        
+    def set_route route                    
         @route = route
         @station = route.first_station         
-        @station.train_arrive (self) 
+        @station.train_arrive self
     end
 
     def go_forward 
         next_station = get_stations.last
         if next_station
-            @station.train_depature self, next_station          
+            @station.train_depature self 
+            next_station.train_arrive self
+            self.station = next_station          
         else                                                    
           puts "The last station! Go back or set the new route"
         end
     end   
 
-    def go_back
+    def go_back 
         previous_station = get_stations.first
         if previous_station
-            @station.train_depature self, previous_station
+            @station.train_depature self 
+            previous_station.train_arrive self
+            self.station = previous_station
         else            
           puts "The first station! Go forward or set the new route"
         end
     end   
  
-    
+   private
+
+   def get_stations                                                # Возвращать предыдущую станцию, текущую, следующую, на основе маршрута                            
+        current_station = @station  
+        if current_station == @route.last_station                   
+            next_station = nil
+        else next_station = @route.stations[@route.stations.index(@station)+1]
+        end
+
+        if current_station == @route.first_station
+            previous_station = nil
+        else  previous_station = @route.stations[@route.stations.index(@station)-1]
+        end
+                    
+        [previous_station, current_station, next_station]
+    end
 end
 
